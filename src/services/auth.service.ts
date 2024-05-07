0b72e0b2b5416cb23710e763feeba78b75ee2811
Ezio Decadt <ezio.decadt@epitech.eu>
2024-05-07 19:32:15 +0200

    feat: Adding user login route

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,28 +1,58 @@
-import connection from '../configs/db';
-import { ResultSetHeader } from 'mysql2';
+import connection from "../configs/db";
+import { ResultSetHeader } from "mysql2";
+import bcrypt from "bcryptjs";
 
 export async function checkIfUserExist(email: string): Promise<boolean> {
-    try {
-        const [rows] = await connection.execute<ResultSetHeader>('SELECT * FROM user WHERE email = ?', [email]);
+  try {
+    const [rows] = await connection.execute<ResultSetHeader>(
+      "SELECT * FROM user WHERE email = ?",
+      [email],
+    );
 
-        if (Array.isArray(rows)) {
-            return rows.length > 0;
-        } else {
-            return false;
-        }
-    } catch (error) {
-        throw error;
+    if (Array.isArray(rows)) {
+      return rows.length > 0;
+    } else {
+      return false;
     }
+  } catch (error) {
+    throw error;
+  }
 }
 
-export async function createUser(email: string, name: string, firstname: string, password: string): Promise<boolean> {
+export async function createUser(
+  email: string,
+  name: string,
+  firstname: string,
+  password: string,
+): Promise<boolean> {
   try {
     const [result] = await connection.execute<ResultSetHeader>(
-      'INSERT INTO user (email, name, firstname, password) VALUES (?, ?, ?, ?)',
+      "INSERT INTO user (email, name, firstname, password) VALUES (?, ?, ?, ?)",
       [email, name, firstname, password],
     );
 
     return result && result.affectedRows === 1;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function checkUserPassword(
+  email: string,
+  password: string,
+): Promise<boolean> {
+  try {
+    const [rows] = await connection.execute<ResultSetHeader>(
+      "SELECT password FROM user WHERE email = ?",
+      [email],
+    );
+
+    if (Array.isArray(rows) && rows.length === 1) {
+      const hashedPassword = rows[0].password;
+      return await bcrypt.compare(password, hashedPassword);
+    } else {
+      return false;
+    }
   } catch (error) {
     throw error;
   }
